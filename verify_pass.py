33851839561ab48e21ff05db0f1b71d502a458da
vigriban <vigriban@tut.by>
2020-06-27 13:31:34 +0300

    Add interactive input

--- verify_pass.py
+++ verify_pass.py
@@ -1,3 +1,5 @@
+import urwid
+
 STRENGTH_POINTS = 2
 
 PASSWORD_VERIFICATORS = [
@@ -17,6 +19,15 @@ def verify_password(password):
         total_score += STRENGTH_POINTS if fn(password) else 0
     return total_score
 
+def on_ask_change(edit, new_edit_text):
+    password_rating = verify_password(new_edit_text)
+    reply.set_text(f"Password rating: {password_rating}")
+
 
 if __name__ == '__main__':
-    pass
+    ask = urwid.Edit("Enter password: ", mask="*")
+    reply = urwid.Text("")
+    menu = urwid.Pile([ask, reply])
+    menu = urwid.Filler(menu, valign='top')
+    urwid.connect_signal(ask, 'change', on_ask_change)
+    urwid.MainLoop(menu).run()
